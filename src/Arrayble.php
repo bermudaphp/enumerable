fa1960b31119b0891acccdd21acf73ca7a59048b
Andrey <alestro@bk.ru>
2020-07-02 11:48:47 +0200

    Create Arrayble.php

---
+++ src/Arrayble.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Bermuda\Enumerable;
+
+
+/**
+ * Interface Arrayble
+ * @package Bermuda\Enumerable
+ */
+interface Arrayble
+{
+    /**
+     * @return array
+     */
+    public function toArray(): array ;
+}
