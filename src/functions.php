917639b4df5f32d476acb51d5e97dcac1db27754
Andrey <alestro@bk.ru>
2020-07-09 17:01:43 +0200

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -10,3 +10,20 @@ function collect(iterable $items = []): Enumerable
 {
     return new Enumerable($items)
 }
+
+/**
+ * @param array $var
+ * @return array
+ */
+function shuffle(array $var): array
+{
+    return usort($var, static function()
+    {
+        if (($x = random_int(0, 100)) == ($y = random_int(0, 100)))
+        {
+            return 0;
+        }
+        
+        return ($x > $y) ? -1 : 1;
+    });
+}
